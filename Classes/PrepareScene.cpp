b29b6cc1f8f1bde18594989a0f8f84a768d20526
vapacity <1580045452@qq.com>
2023-12-10 21:21:25 +0800

    02-zwj
    完成棋格创建
    
    完成鼠标监控，位置高亮棋格
    
    完成界面大概设计

---
+++ Classes/PrepareScene.cpp
@@ -0,0 +1,72 @@
+// PrepareScene.cpp
+#include "cocos2d.h"
+#include "PrepareScene.h"
+#include "HexGrid.h"
+USING_NS_CC;
+
+Scene* PrepareScene::createScene()
+{
+    // 创建一个场景对象，该对象将由自动释放池自动释放
+    auto scene = PrepareScene::create();
+    return scene;
+}
+bool PrepareScene::init()
+{
+    // 首先调用基类的init方法
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    // 这里可以添加初始化场景的代码
+    // 初始化背景
+    initBackground();
+
+    // 初始化棋格
+    inithexGrid();
+
+    // 创建鼠标监听
+    auto mouseListener = EventListenerMouse::create();
+    mouseListener->onMouseMove = CC_CALLBACK_1(PrepareScene::onMouseMove, this);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(mouseListener, this);
+
+    return true;
+}
+
+void PrepareScene::inithexGrid()
+{
+    hexGrid = HexGrid::create(14, 8); // 假设您的棋盘有8行8列
+    this->addChild(hexGrid, 0);
+}
+
+void PrepareScene::initBackground()
+{
+    backgroundImg=Sprite::create("battle-background3.png");
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    backgroundImg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    backgroundImg->setScaleX(visibleSize.width / backgroundImg->getContentSize().width);
+    backgroundImg->setScaleY(visibleSize.height / backgroundImg->getContentSize().height);
+    this->addChild(backgroundImg, -1);
+}
+
+void PrepareScene::onMouseMove(Event* event)
+{
+    // 获取鼠标事件
+    EventMouse* mouseEvent = dynamic_cast<EventMouse*>(event);
+
+    // 获取鼠标位置
+    Vec2 mousePosition = mouseEvent->getLocationInView();
+    static HexCell* lastCell = nullptr;
+    auto currentCell = hexGrid->getCellAt(mousePosition);
+
+    if (currentCell != lastCell) {
+        if (currentCell) {
+            currentCell->changeOpacity(150); // 新的棋格增加透明度
+        }
+        if (lastCell) {
+            lastCell->changeOpacity(80); // 前一个棋格恢复透明度
+        }
+        lastCell = currentCell;
+    }
+}
---
+++ Classes/PrepareScene.h
@@ -0,0 +1,31 @@
+// PrepareScene.h
+
+#ifndef __PREPARE_SCENE_H__
+#define __PREPARE_SCENE_H__
+
+#include "cocos2d.h"
+#include "HexGrid.h"
+USING_NS_CC;
+class PrepareScene : public cocos2d::Scene
+{
+public:
+    Sprite* backgroundImg;
+    HexGrid* hexGrid;
+public:
+    // 创建场景的静态方法
+    static cocos2d::Scene* createScene();
+    
+
+    // 初始化场景的方法
+    virtual bool init();
+    // 创建背景图片
+    void initBackground();
+    // 创建棋格
+    void inithexGrid();
+    // 实现 "static create()" 方法，用于创建对象
+    CREATE_FUNC(PrepareScene);
+
+    void onMouseMove(Event* event);
+};
+
+#endif // __PREPARE_SCENE_H__
